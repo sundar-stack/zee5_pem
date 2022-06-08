29693dfdca49ddd1ade01ba196936b1501e7d0ad
sundar <sundercheery42@gmail.com>
2022-06-08 09:58:41 +0530

    initial setup

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "./.env" });
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL, (err) => {
+  if (err) {
+    console.log("mongo error", err);
+  } else {
+    console.log("Successfully connected to DB");
+  }
+});
+
+//route setup
+const router = require("./routes/routeHandler");
+const { API_ENDPOINT_CONFIG } = require("./constants/ApiConfig");
+const globalErrorHandler = require("./controllers/ErrorController");
+app.use(API_ENDPOINT_CONFIG.BASE_END_POINT, router);
+
+///global error handler
+app.use(globalErrorHandler);
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`server started on ${PORT}`);
+});
